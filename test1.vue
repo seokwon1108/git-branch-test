8f65d608f497b9e0aa3e5c6c2f8356084a564763
seokwon1108 <swseong@vvnst.com>
2024-06-05 16:30:00 +0900

    test1 이전 히스토리 파일변경 테스트

--- test1.vue
+++ test1.vue
@@ -1,5 +1,12 @@
 <template>
   <div>
+    <header class="header">헤더</header>
+    <main class="main">
+      메인
+      <nav class="nav">내비게이션</nav>
+      <article class="article">아티클</article>
+    </main>
+    <footer class="footer">푸터</footer>
     <header>헤더</header>
     <main>
       <section class="section-1 hello">섹션 1</section>
